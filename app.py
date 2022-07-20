b2764dbd0867183e736ec3fc4d13e26ba42a92d9
derrickpriebe <dpriebe@gmail.com>
2022-07-19 21:33:46 -0700

    Important fixes for row reference and filtering

--- app.py
+++ app.py
@@ -1,25 +1,35 @@
-from flask import Flask, render_template, json, redirect, request
+from flask import Flask, jsonify, render_template, json, redirect, request
 import pandas as pd 
- 
+
 # Flask constructor
 app = Flask(__name__)
- 
+
 # Data setup
-salary_table = pd.read_csv("salaries.csv")
-salary_table.fillna('', inplace=True)
-headings = list(salary_table.columns)
-print(headings)
-data = list(salary_table.values)
-print(data)
+table = pd.read_csv("salaries.csv")
+table.fillna('', inplace=True)
+table_dict = {col: list(table[col]) for col in table.columns}
+headings = list(table.columns)
+data = list(table.values)[0:10]
+print(data[0])
 
 # Homepage URL call
 @app.route("/")
 def home():
     return render_template("home.html", headings=headings, data=data)
 
-@app.route('/GetPlayerInfo/<row>')
-def GetPlayerInfo(row):
-    return row + "something"
+@app.route('/GetPlayerInfo/<int:rowindex>')
+def get_player_info(rowindex):
+    return jsonify(list(data[rowindex]))
+
+@app.route('/search/', methods=["POST"])
+def search():
+    #print(request.form.get)
+    name = request.form.get("name")
+    print(name)
+    team = request.form.get("team")
+    print(team)
+    filterdata = []
+    return render_template("home.html", headings=headings, data=filterdata)
 
 # Listener
 if __name__ == "__main__":
