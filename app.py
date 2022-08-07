838229f1d0a41fb598a2ae3f58a44dae0f598350
derrickpriebe <dpriebe@gmail.com>
2022-08-07 13:28:29 -0700

    Incorporate microservice

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, render_template, redirect, request, url_for
 import pandas as pd 
+import webbrowser
+import zmq
 
 # Flask constructor
 app = Flask(__name__)
@@ -7,38 +9,53 @@ app = Flask(__name__)
 # Data setup
 table = pd.read_csv("salaries.csv")
 table.fillna('', inplace=True)
-table_dict = {col: list(table[col]) for col in table.columns}
 headings = list(table.columns)
 data = list(table.values)
+
+# Zeromq setup and socket connection
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5555")
 
 # Homepage URL call
 @app.route("/")
 def home():
     return render_template("home.html", headings=headings, data=data)
 
+# Route to get additional player info via "info" link
 @app.route('/GetPlayerInfo/<int:rowindex>')
 def get_player_info(rowindex):
-    return jsonify(list(data[rowindex-1]))
+    # Send and receive message request
+    socket.send_string(str(rowindex))
+    message = socket.recv_string()
+    # Access url from message and open brower tab to resolve url
+    webbrowser.open(message)
+    return redirect(url_for("home"))
 
+# Route to post search results
 @app.route("/", methods=["POST"])
 def search():
+    # Collect search variables
     search = request.form.get("search")
     name = request.form.get("name")
     name2 = request.form.get("name2")
     team = request.form.get("team")
     filterdata = []
-    for e in data:
-        if ((search == "" or search.lower() in e[0].lower() 
-            or search.lower() in e[1].lower() 
-            or search.lower() in e[2].lower()) and
-            (name == "" or name.lower() in e[0].lower()) and
-            (name2 == "" or name2.lower() in e[1].lower()) and
-            (team == "" or team.lower() in e[2].lower())):
-            filterdata.append(e)
+    # Conduct search on data
+    for element in data:
+        if ((search == "" or search.lower() in element[0].lower() 
+            or search.lower() in element[1].lower() 
+            or search.lower() in element[2].lower()) and
+            (name == "" or name.lower() in element[0].lower()) and
+            (name2 == "" or name2.lower() in element[1].lower()) and
+            (team == "" or team.lower() in element[2].lower())):
+            filterdata.append(element)
     if filterdata == []:
+        # If no search results, show message
         no_data = ["No  "]
         return render_template("home.html", headings=no_data, name=name, name2=name2, team=team)
     else:
+        # If search results, show results
         return render_template("home.html", headings=headings, data=filterdata,  name=name, name2=name2, team=team)
 
 # Listener
